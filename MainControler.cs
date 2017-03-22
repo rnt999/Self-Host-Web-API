f8a2a93a55d4a1541973aa118092ee4f8445e5a7
Renatas Daunys <renatas.daunys@gmail.com>
2017-03-22 21:19:37 +0000

    Playing with self host

---
+++ MainControler.cs
@@ -0,0 +1,47 @@
+﻿using Owin;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OwinSelfhostSample
+{
+
+    public abstract class BaseController : ApiController
+    {
+        public BaseController()
+        {
+            OwinRequestScopeContext.Current.Items["Data"] = "Test Data";
+        }        
+    }
+
+    public class MainController : BaseController
+    {
+        // GET api/values 
+        public IEnumerable<string> Get()
+        {
+            var value = OwinRequestScopeContext.Current.Items["Data"].ToString();
+            return new string[] { "value1", value };
+        }
+
+        // GET api/values/5 
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values 
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5 
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using OwinSelfhostSample;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,20 @@ namespace Self_Host_Web_API
     {
         static void Main(string[] args)
         {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/Main").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
         }
     }
 }
